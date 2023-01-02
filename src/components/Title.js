b58b6f39f78d57fa8ad785145cd3b5a28e74626c
Rima Das <rimadas.automationtester@gmail.com>
2023-01-01 23:08:09 -0800

    Updating resume page

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title() {
     return (
         <div class="welcomePage">
-            <h1 class='nameTitle'>Hello,I am Rima Das</h1>
+            <li></li><h1 class='nameTitle'>Hello,I am Rima Das</h1>
         </div>
     );
 } 
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,9 +2,42 @@ import React from "react";
 
 export default function Resume(){
     return (
-        <div>
-            <h1>Resume</h1>
-            <a href="Rima_Das.pdf">Download Resume</a>
+        <div class="resumePage">
+            <ul>
+            <h2>Resume</h2>
+            <hr></hr>
+            <div class="resumePDF">
+            <li><a href="Rima_Das.pdf">Download Resume</a></li>
+            </div>
+            <br></br>
+            <br></br>
+            <h2>Front-end Proficiencies</h2>
+            <hr></hr>
+            <div class="frontEndText">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+            </div>
+            <br></br>
+            <br></br>
+            <h2>Back-end Proficiencies</h2>
+            <hr></hr>
+            <div class="backEndText">
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL</li>
+            <li>Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>Rest</li>
+            <li>GraphQL</li>
+            </div>
+            </ul>
+            
+            
         </div>
     );
 }
